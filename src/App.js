58f3d47c7bb52c7a5a1f31567e97f629a1175143
Denzel Turambi <dturambi26@gmail.com>
2023-10-26 13:39:55 -0600

    feat: Add counter

--- src/App.js
+++ src/App.js
@@ -2,10 +2,19 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  function clickCounter() {
+    return setCounter(counter + 1);
+  }
+
   return (
     <div className="app" ontouchstart="">
-      <div class="button">
+      <div class="button" onClick={clickCounter}>
         <p>Click Me!</p>
+      </div>
+      <div className="counter-text">
+        {counter === 69 || counter === 420 ? <p>clicks: {counter} hehe</p> : <p>clicks: {counter}</p>}
       </div>
     </div>
   );
